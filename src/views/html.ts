493c4e26fc94b0fa783f0706873af1ea14fcf9fd
Jeswin Kumar <jeswinpk@agilehead.com>
2017-12-18 23:20:38 +0530

    Remove purecss layouts

--- src/views/html.ts
+++ src/views/html.ts
@@ -1,4 +1,7 @@
 type Args = { title: string, content: string };
+
+import sidebar from "./sidebar";
+
 export default function({ title, content }: Args): string {
   return `
     <!doctype html>
@@ -15,21 +18,7 @@ export default function({ title, content }: Args): string {
       </head>
       <body>
         <div id="layout" class="pure-g">
-          <div class="sidebar pure-u-1 pure-u-md-1-4">
-              <div class="header">
-                  <h1>NullAuth</h1>
-                  <p>
-                    Password-less Authentication and Access Delegation
-                  </p>
-                  <ul>
-                      <li><a href="https://www.nullauth.org">Specification</a></li>
-                      <li><a href="https://www.nullauth.org/downloads">Downloads</a></li>
-                      <li><a href="https://www.nullauth.org/examples">Examples</a></li>
-                      <li><a href="https://github.com/jeswin/nullauth/issues">Discussion</a></li>
-                  </ul>
-              </div>
-          </div>
-
+          ${sidebar()}
           <div class="content pure-u-1 pure-u-md-3-4">
           ${content}
           </div>
---
+++ src/views/sidebar.ts
@@ -0,0 +1,16 @@
+export default function(): string {
+  return `
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+      <div class="header">
+          <h1>NullAuth</h1>
+          <p>Password-less Authentication<br />and Access Delegation</p>
+          <ul>
+              <li><a href="https://www.nullauth.org">Specification</a></li>
+              <li><a href="https://www.nullauth.org/downloads">Downloads</a></li>
+              <li><a href="https://www.nullauth.org/examples">Examples</a></li>
+              <li><a href="https://github.com/jeswin/nullauth/issues">Discussion</a></li>
+          </ul>
+      </div>
+    </div>
+  `;
+}
